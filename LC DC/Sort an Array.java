02b4f8dbba9e941e7e0081fbbb8f8d1d00df2ae3
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-07-25 19:00:15 +0530

    Create Sort an Array.java

---
+++ LC DC/Sort an Array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+
+    }
+   private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low;      
+        int right = mid + 1;   
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2 ;
+        mergeSort(arr, low, mid);  
+        mergeSort(arr, mid + 1, high); 
+        merge(arr, low, mid, high); 
+    }
+}
